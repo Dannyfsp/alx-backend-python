74310517fbea6c558deef5955f63678c6c7fe88e
Dannyfsp <femoscopilala@gmail.com>
2023-05-23 08:19:48 +0000

    testing that the result of _public_repos_url is the expected one based on the mocked payload

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,9 +5,10 @@ Module for testing client
 
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -27,3 +28,16 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+    def test_public_repos_url(self):
+        '''
+        Test that the result of _public_repos_url is the expected one based
+        on the mocked payload
+        '''
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {'repos_url': 'World'}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload['repos_url'])
